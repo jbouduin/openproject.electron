ce393a3d85ba6440e427910069a21ffb3d94e650
Johan Bouduin <johan@der-e-coach.de>
2020-05-14 18:04:22 +0200

    ignore client-settings

--- src/main/@core/client-settings.ts
+++
@@ -1,6 +0,0 @@
-// NEVER commit this file with sensitive data in it !!!
-export class ClientSettings {
-  public static apiKey = '<Your API key>';
-  public static apiHost = '<Your host>';
-  public static apiRoot = 'api/v3';
-}
