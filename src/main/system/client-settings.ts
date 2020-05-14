0e7d6356f702ab7ed1010a1af787a884913459ed
Johan Bouduin <johan@der-e-coach.de>
2020-05-14 14:43:57 +0200

    Commit file separately and .gitignore it afterwards as it contains sensitive data

---
+++ src/main/system/client-settings.ts
@@ -0,0 +1,5 @@
+export class ClientSettings {
+  public static apiKey = '<Put your API Key here>';
+  public static apiHost = '<Put your host here>';
+  public static apiRoot = '<Put your API root here>';
+}
